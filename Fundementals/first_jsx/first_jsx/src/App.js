fad41ff069e9e48a6497039643c0fcd3ec276a9e
swynnejr <saucersam@gmail.com>
2021-09-01 13:59:13 -0500

    Prop it up

--- Fundementals/first_jsx/first_jsx/src/App.js
+++ Fundementals/first_jsx/first_jsx/src/App.js
@@ -1,18 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello Dojo!</h1>
+//       <h3>Things I need to do:</h3>
+//       <ul>
+//         <li>Learn React</li>
+//         <li>Climb Mt Everest</li>
+//         <li>Run a marathon</li>
+//         <li>Feed the dogs</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+  
+import React from "react";
+import PeopleComponent from "./components/someclasscomponent";
+
+
+class Person {
+  constructor(firstName, lastName, age, hairColor) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.hairColor = hairColor;
+  }
+}
+
+var jane = new Person("Jane", "Doe", 45, "Black");
+var john = new Person("John", "Smith", 88, "Brown");
+var millard = new Person("Millard", "Fillmore", 50, "Brown");
+var marla = new Person("Marla", "Smith", 62, "Brown");
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Dojo!</h1>
-      <h3>Things I need to do:</h3>
-      <ul>
-        <li>Learn React</li>
-        <li>Climb Mt Everest</li>
-        <li>Run a marathon</li>
-        <li>Feed the dogs</li>
-      </ul>
-    </div>
+      <div className="App">
+          <PeopleComponent person={jane} />
+          <PeopleComponent person={john} />
+          <PeopleComponent person={millard} />
+          <PeopleComponent person={marla} />
+      </div>
   );
 }
 
--- Fundementals/first_jsx/first_jsx/src/components/SomeClassComponent.js
+++ Fundementals/first_jsx/first_jsx/src/components/SomeClassComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-    
+
     
 class SomeClassComponent extends Component {
     render() {
@@ -21,4 +21,19 @@ class Header extends React.Component {
         }
     }
 
-    export default Header;
+export default Header;
+
+class PeopleComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    {this.props.person.lastName},{this.props.person.firstName}
+                </h1>
+                <p>age: {this.props.person.age}</p>
+                <p>hair color: {this.props.person.hairColor}</p>
+            </div>
+        );
+    }
+}
+export default PeopleComponent;
