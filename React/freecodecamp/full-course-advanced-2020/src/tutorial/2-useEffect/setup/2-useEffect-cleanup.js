c7e0fe2f94e4ab7ecb347de2e9f358d75a233d37
swynnejr <saucersam@gmail.com>
2021-09-07 17:22:15 -0500

    useEffect basics

--- React/freecodecamp/full-course-advanced-2020/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ React/freecodecamp/full-course-advanced-2020/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,24 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+  console.log(size);
+
+  const checkSize = () =>{
+    setSize(window.innerWidth)
+  }
+
+  useEffect(()=>{
+    window.addEventListener('resize',checkSize)
+  })
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size} PX</h2>
+    </>
+
+  )
+
 };
 
 export default UseEffectCleanup;
